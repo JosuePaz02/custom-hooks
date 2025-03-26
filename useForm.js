d2a3684c99312ce12f1481d8c3e149066c6a0cde
Josue Paz <132326243+JosuePaz02@users.noreply.github.com>
2025-03-26 17:10:37 -0600

    first commit

---
+++ useForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export const useForm = (initialForm = {}) => {
+    const [formState, setFormState] = useState(initialForm)
+    const onChange = ({target}) => {
+        const {name, value} = target
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
+    const onReset = () => {
+        setFormState(initialForm)
+    }
+    return {
+        ...formState,
+        onChange,
+        onReset
+  }
+}
